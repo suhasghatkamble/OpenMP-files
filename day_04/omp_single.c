e079047092d48e9971cab59f8a3a6302246202bf
suhasghatkamble <167551759+suhasghatkamble@users.noreply.github.com>
2024-05-27 18:45:59 +0530

    Add files via upload

---
+++ day_04/omp_single.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<omp.h>
+#define N 100
+int main() {
+	int a[N];
+	int sum = 0;
+#pragma omp parallel num_threads(N)
+	{
+
+		int i = omp_get_thread_num();
+		a[i] = i + 1;
+#pragma omp barrier
+#pragma omp single
+		{
+	printf("thread is: %d \n", omp_get_thread_num());
+			for(int j = 0; j < N;j++){
+				sum+= a[j];
+			}
+		}
+	}
+	printf("Sum = %d \n", sum);
+	return 0;
+}
+
